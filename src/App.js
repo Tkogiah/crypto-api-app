d2775c90345704b7784265e2409e9fcd482ed6a9
Marcus Overstreet <tkogiah@gmail.com>
2022-08-05 03:33:37 -0700

    added eslint ignore

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ export default function App() {
     fetch('https://api2.binance.com/api/v3/ticker/24hr')
     .then(response => response.json())
     .then(data => {
-      const filteredData = data.filter((crypto,i,data) => {
+      // eslint-disable-next-line
+      const filteredData = data.filter((crypto) => {
         if( Object.keys(CRYPTO_NAMES).includes(crypto.symbol)) {
           return true
         }
